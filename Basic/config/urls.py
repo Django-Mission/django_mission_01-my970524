15c8df4663aea98ecdf4534c432cf483e5c25675
minji <my970524@naver.com>
2022-03-30 16:02:19 +0900

    feat: lotto 추첨 기능
    1. demos 앱 생성
    2. demos/views.py에 lotto 함수 구현
    3. config/urls.py에 'lotto/' url 생성

--- Basic/config/urls.py
+++ Basic/config/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from demos.views import lotto
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('lotto/', lotto, name='lotto'),
 ]
---
+++ Basic/demos/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+# Create your views here.
+def lotto(request):
+    pull_num = [x for x in range(1, 46)]
+    lotto_num = random.sample(pull_num, 6)
+    print(lotto_num)
+
+    return HttpResponse(lotto_num)
