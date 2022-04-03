46f374217cb2b066a42bfadfff4526a261d3d77c
minji <my970524@naver.com>
2022-04-03 18:36:10 +0900

    refactor: 코드 수정
    1. Basic/demos/views.py의 디버깅 라인 주석처리
    2. Basic/demos/templates lotto.html 수정

--- Basic/demos/views.py
+++ Basic/demos/views.py
@@ -6,6 +6,6 @@ import random
 def lotto(request):
     pull_num = [x for x in range(1, 46)]
     lotto_num = random.sample(pull_num, 6)
-    print(lotto_num)
+    # print(lotto_num)
 
     return render(request, 'lotto.html', {'lotto_num': lotto_num})
